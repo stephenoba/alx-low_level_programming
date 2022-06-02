8170f5170138191caec03e92aec99ac0ec48e4ba
Stephen Oba <obastepheno@gmail.com>
2022-06-03 00:21:53 +0100

    added C file 101-quote.c to write to the stderror

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 1
+*/
+int main(void)
+{
+	char strg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(2, strg, sizeof(strg));
+
+	return (1);
+}
