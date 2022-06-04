8652c918e244df4f4998e62f301df2992def2546
Stephen Oba <obastepheno@gmail.com>
2022-06-04 13:22:08 +0100

    added C progam to print all letters in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 97;
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
