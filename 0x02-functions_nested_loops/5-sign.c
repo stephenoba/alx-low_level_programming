5f290874cb711db80f4adb1323d44404d4d9f157
Stephe Oba <obastepheno@gmail.com>
2022-06-08 00:27:59 +0100

    added program to print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - print the sign of a number.
+ *
+ * Return: 1 (positive) -1 (negeive) 0 (zero).
+ */
+
+int print_sign(int n)
+{
+	if (n  == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
