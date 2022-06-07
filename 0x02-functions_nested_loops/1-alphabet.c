222917ec2f6c4d9e936c10a81a59f9abe1b34c43
Stephen Oba <obastepheno@gmail.com>
2022-06-07 22:51:08 +0100

    added C programme to print lowercase alphabets from a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Uses _putchar to write lowercase
+ * alphabets
+ * 
+ */
+void print_alphabet(void)
+{
+        int n = 97;
+
+        while (n <= 122)
+        {
+                _putchar(n);
+                n++;
+        }
+        _putchar('\n');
+        return;
+}
+
