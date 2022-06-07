928f9cc504cbbaa5d15bed40b64c722abe8291b3
Stephen Oba <obastepheno@gmail.com>
2022-06-07 23:33:48 +0100

    added program to print lowercase alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Uses _putchar to write alphabets 10x
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int n;
+
+	i = 0;
+	while (i < 10)
+	{
+		n = 97;
+		while (n <= 122)
+		{
+			_putchar(n);
+			n++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
