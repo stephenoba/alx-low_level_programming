93ecd5cc46a01d6d502aac4cfa4284dad691b91a
Stephen Oba <obastepheno@gmail.com>
2022-06-09 09:21:56 +0100

    fixec the errors on task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* positive - function to check if a number is + or -
+*
+* Return: Always 0
+*/
+void positive_or_negative(int i)
+{
+	if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+}
