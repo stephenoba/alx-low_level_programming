7d95a82d32ee1965317ec2d59bb9f514cad58029
Stephe Oba <obastepheno@gmail.com>
2022-06-09 19:29:08 +0100

    added program to multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplt two digits
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int result
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
