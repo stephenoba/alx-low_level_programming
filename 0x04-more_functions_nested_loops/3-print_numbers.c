30a2ec715f358dd14f6b70bcdd127489ee07829b
Stephe Oba <obastepheno@gmail.com>
2022-06-09 19:50:27 +0100

    added program to prind numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ */
+void print_numbers(void)
+{
+	int n;
+
+	n = 48;
+	while (n <= 57)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
