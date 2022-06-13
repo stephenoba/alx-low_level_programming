43de778e20d3d18f7dbc21d00c429ace5cb4c5af
Stephe Oba <obastepheno@gmail.com>
2022-06-13 19:59:55 +0100

    fixed program with unvalud output

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,20 +7,14 @@
  */
 void puts2(char *str)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (str[i])
+	int i = 0;
+	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 		i++;
-	}
-
-	j = 0;
-	while (j <= i)
-	{
-		_putchar(str[j]);
-		j += 2;
 	}
 	_putchar('\n');
 }
