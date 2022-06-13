7eb134f52eb72dd84cb16a0259b69be43ecf54aa
Stephe Oba <obastepheno@gmail.com>
2022-06-13 21:17:03 +0100

    made corrections to file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,31 +2,27 @@
 
 /**
  * puts_half - prints half of a string
- * 
  * @str: string to modify
  */
 void puts_half(char *str)
 {
 	int c;
 
-	if (*str)
+	c = 0;
+	while (str[c])
+		c++;
+	if (c % 2 == 1)
 	{
-		c = 0;
-		while (str[c])
-			c++;
-		if (c % 2 == 1)
-		{
-			c = (c + 1) / 2;
-		}
-		else
-		{
-			c = c / 2;
-		}
-		while (str[c])
-		{
-			_putchar(str[c]);
-			c++;
-		}
-		_putchar('\n');
+		c = (c + 1) / 2;
 	}
+	else
+	{
+		c = c / 2;
+	}
+	while (str[c])
+	{
+		_putchar(str[c]);
+		c++;
+	}
+	_putchar('\n');
 }
