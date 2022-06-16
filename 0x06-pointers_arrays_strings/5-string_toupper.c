06831bd5bdfa0ee2e43a559b5be7fba31f2a7ccf
Stephen Oba <obastepheno@gmail.com>
2022-06-16 21:17:44 +0100

    added program to convert lowercase letters in a string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,4 +5,16 @@
  * @s: string
  * Return: pointer to string
  */
-char *string_toupper(char *s);
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
