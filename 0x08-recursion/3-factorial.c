dd41ca7b951ed7c067c7ac6981bb49e778a91c02
Stephen Oba <obastepheno@gmail.com>
2022-06-22 23:39:51 +0100

    fixed error with expected output

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,10 +8,12 @@
 int factorial(int n)
 {
 	if (n < 0)
+	{
 		return (-1);
-	if (n == 0)
+	}
+	else if (n == 0)
+	{
 		return (1);
-	if (n == 1)
-		return (n)
+	}
 	return (n * factorial(n - 1));
 }
