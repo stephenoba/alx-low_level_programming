aab12e156d1373bc2069f4a541ae93abd22146e2
Stephen Oba <obastepheno@gmail.com>
2022-06-23 00:11:02 +0100

    fixed error with prototype

--- 0x08-recursion/5-sort_recursion.c
+++ 0x08-recursion/5-sort_recursion.c
@@ -1,16 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: number
- * Return: square root
- */
-int _sqrt_recursion(int n)
-{
-	return (sqrt_(n, 1));
-}
-
-/**
  * sqrt_ - recursion
  * @n: int
  * @i: int
@@ -18,11 +8,21 @@ int _sqrt_recursion(int n)
  */
 int sqrt_(int n, int i)
 {
-	if (n < 0)
-		return (-1);
-	if ((i * i) > n)
-		return (-1);
-	if (i * i == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+        if (n < 0)
+                return (-1);
+        if ((i * i) > n)
+                return (-1);
+        if (i * i == n)
+                return (i);
+        return (_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number
+ * Return: square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_(n, 1));
 }
