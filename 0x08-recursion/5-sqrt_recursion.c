6383bf052839e0d3afa87e20bd2199b9721e3cfd
Stephen Oba <obastepheno@gmail.com>
2022-06-23 00:22:03 +0100

    fixed error

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,19 +2,17 @@
 
 /**
  * sqrt_ - recursion
- * @n: int
- * @i: int
+ * @a: int
+ * @b: int
  * Return: square root
  */
-int sqrt_(int n, int i)
+int sqrt_(int a, int b)
 {
-        if (n < 0)
-                return (-1);
-        if ((i * i) > n)
-                return (-1);
-        if (i * i == n)
-                return (i);
-        return (_sqrt(n, i + 1));
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt_(a, b + 1));
 }
 
 /**
@@ -23,6 +21,4 @@ int sqrt_(int n, int i)
  * Return: square root
  */
 int _sqrt_recursion(int n)
-{
-	return (sqrt_(n, 1));
-}
+	return (sqrt_(n, 0));
