9194f5fbfbfdbf5302e30da354767ea4bfa575c8
Stephe Oba <obastepheno@gmail.com>
2022-06-26 17:30:56 +0100

    added program that prints its firzt argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: integer success(0) failure(!0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (EXIT_SUCCESS);
+}
