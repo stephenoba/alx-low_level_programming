69d84dadb6b12f22557bbd97427752e0d2868d6b
Stephen Oba <obastepheno@gmail.com>
2022-06-26 19:05:09 +0100

    fixed error with output

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc <= 1 || argc >= 4 || !_isnumber(argv[1]) || !_isnumber(argv[2]))
+	if (argc <= 1 || argc >= 4)
 	{
 		printf("Error\n");
 		return (EXIT_FAILURE);
@@ -19,30 +19,3 @@ int main(int argc, char *argv[])
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (EXIT_SUCCESS);
 }
-
-/**
- * _isnumber - checks if a string is a number
- * @s: string
- * Return: 1 if string is a number else 0
- */
-int _isnumber(char s[])
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (!isdigit(s[i]))
-		{
-			if (s[0] == 45)
-			{
-				i++;
-				continue;
-			}
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
-
