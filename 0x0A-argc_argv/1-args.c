6c7fff1dcee62d368bec98d0e4fbf7029dacf6e9
Stephen Oba <obastepheno@gmail.com>
2022-06-26 17:45:08 +0100

    added program to print out the number of attribute passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: integer success(0) failure(!0)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (EXIT_SUCCESS);
+}
