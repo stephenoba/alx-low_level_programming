b9a7fca5012a65571e526d09a1f91a4a2f8527bb
Stephen Oba <obastepheno@gmail.com>
2022-06-26 17:40:26 +0100

    fixed error with requirements

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * @argv: arguments
  * Return: integer success(0) failure(!0)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (EXIT_SUCCESS);
