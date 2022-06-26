7c0392981a41d28a63c2db6906deef925db28b3a
Stephen Oba <obastepheno@gmail.com>
2022-06-26 17:50:30 +0100

    added program to print out all arguments and name

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: integer success(0) failure(!0)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (EXIT_SUCCESS);
+}
