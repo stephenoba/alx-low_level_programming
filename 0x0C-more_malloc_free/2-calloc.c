dd00f85ea4bf78647db60b1208423725966c0ec3
Stephen Oba <obastepheno@gmail.com>
2022-06-30 17:46:06 +0100

    fixed betty related warnings

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,10 +13,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; i < nmemb; i++)
 	{
 		p[i] = 0;
