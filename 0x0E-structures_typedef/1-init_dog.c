d21d757556bcb2f1dca77ee9cadabeba5ecfdfd0
Stephen Oba <obastepheno@gmail.com>
2022-07-04 23:34:16 +0100

    added program to initialize a dog structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
