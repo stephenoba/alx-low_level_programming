399c5ef13e8f5cfa5ee3471da934818305ef14cf
Stephen Oba <obastepheno@gmail.com>
2022-07-04 23:18:44 +0100

    created directory and added doh header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct to create a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
