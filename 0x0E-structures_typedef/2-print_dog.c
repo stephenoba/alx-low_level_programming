d57b675eb6a5d969a234a205cbd0139cd26bf119
Stephen Oba <obastepheno@gmail.com>
2022-07-05 00:41:28 +0100

    task three

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the vallues of a dog structure
+ * @d: dog structure
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\nAge: %f\nOwner: %s\n",
+				d->name != NULL ? d->name : "(nil)",
+				d->age,
+				d->owner != NULL ? d->owner : "(nil)");
+	}
+}
