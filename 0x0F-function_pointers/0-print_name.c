959133dff509f8f71e2284c3c55b77c560e7f732
Stephen Oba <obastepheno@gmail.com>
2022-07-07 01:09:00 +0100

    fixed incorrect output

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,11 @@
  * print_name - prints a name
  * @name: pointer to name
  * @f: callback function
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name || f)
 		f(name);
+	return;
 }
