6f9e1e6f6f150dbbc5c440c198bf734dc661de2c
Stephen Oba <obastepheno@gmail.com>
2022-07-07 03:07:58 +0100

    fixed requirements error

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,16 +3,17 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: argument count
- * @argv: argument list
- * Return: 0 (success) -1 (failure)
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int a1, a2, res;
-	char op;
-	int (*f)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
@@ -20,27 +21,28 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	a1 = atoi(argv[1]);
-	a2 = atoi(argv[3]);
-	op = *argv[2];
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-	f = get_op_func(argv[2]);
+	func = get_op_func(argv[2]);
 
-	if (!f)
+	if (!func)
 	{
-		printf("Error no f\n");
+		printf("Error\n");
 		exit(99);
 	}
 
-	if ((op == '/' || op == '%') && a2 == 0)
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
 	{
-		printf("Error 0 divisor\n");
+		printf("Error\n");
 		exit(100);
 	}
 
-	res = f(a1, a2);
+	result = func(arg1, arg2);
 
-	printf("%d\n", res);
+	printf("%d\n", result);
 
 	return (0);
 }
