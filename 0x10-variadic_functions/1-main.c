e7dea602c02ffb7ba85019c26ccf079523b9aa2b
Stephen Oba <obastepheno@gmail.com>
2022-07-08 16:46:22 +0100

    fixed errored output

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -8,5 +9,7 @@
 int main(void)
 {
     print_numbers(", ", 4, 0, 98, -1024, 402);
+    print_numbers(NULL, 4, 304, 76, 87236, 28376);
+    print_numbers("; ", 0);
     return (0);
 }
