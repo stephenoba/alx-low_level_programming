494c375e2921851f8ceea705c44a02a878414d1c
Stephen Oba <obastepheno@gmail.com>
2022-07-08 16:36:58 +0100

    added program to print numbers with a separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+  * print_numbers - sums all integers passed to a function
+  * @seperator: seperator to print between numbers
+  * @n: number of arguments
+  * Return: sum
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+
+	if (separator != NULL)
+	{
+		va_start(args, n);
+
+		while (i < n)
+		{
+			printf("%d", va_arg(args, int));
+			if (i != n - 1)
+				printf("%s", separator);
+			i++;
+		}
+
+		va_end(args);
+		printf("\n");
+	}
+}
