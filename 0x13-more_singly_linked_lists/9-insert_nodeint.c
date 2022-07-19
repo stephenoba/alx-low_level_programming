1041560d30c60b675d6909d85621ec7495fbbe9b
Stephen Oba <obastepheno@gmail.com>
2022-07-19 18:50:28 +0100

    fixed errored out put

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,13 +13,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || head == NULL)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
 	if (current == NULL || idx == 0)
 	{
-		new_node->next = NULL;
 		*head = new_node;
 		return (new_node);
 	}
