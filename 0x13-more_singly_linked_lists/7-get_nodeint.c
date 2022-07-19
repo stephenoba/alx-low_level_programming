c2ab996434d5d0ade8c7aac919906e1af3786fbb
Stephen Oba <obastepheno@gmail.com>
2022-07-19 10:59:41 +0100

    added function to get node at an index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get a node at an index (node starts at 0)
+ * @head: pointer to first node
+ * @index: index
+ * Return: node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+	while (head !=  NULL)
+	{
+		if (head->next)
+		{
+			if (i == index)
+				return (head);
+			head = head->next;
+			i++;
+		}
+	}
+	return (NULL);
+}
