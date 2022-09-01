7300dc1d3542abb60b49cb269f3896af675e23c6
Stephen Oba <obastepheno@gmail.com>
2022-09-01 13:30:59 +0100

    Add header file and 0-print_dlistint.c
    0-print_dlistint.c contains function print_dlistint, that prints
    all elements in a doubly linked list and return the count of nodes

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the elements in a dlistint_t
+ * @h: head of list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+        size_t len = 0;
+
+        while (h)
+        {
+                printf("%d", h->n);
+                h = h->next;
+                len++;
+        }
+        return (len);
+}
