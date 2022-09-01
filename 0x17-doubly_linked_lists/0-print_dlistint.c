8ae2ea9082fa524de473c66527ae62ac12e3daeb
Stephen Oba <obastepheno@gmail.com>
2022-09-01 13:48:59 +0100

    Fix betty warning

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-		size_t len = 0;
+	size_t len = 0;
 
-		while (h)
-		{
-				printf("%d\n", h->n);
-				h = h->next;
-				len++;
-		}
-		return (len);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		len++;
+	}
+	return (len);
 }
