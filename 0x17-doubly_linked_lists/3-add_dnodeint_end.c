ce65f089f0f4c845ddb2067a2a8f61061d0bbdec
Stephen Oba <obastepheno@gmail.com>
2022-09-01 14:51:22 +0100

    Create function add_nodeint_end
    Function adds node to the end of a dlistint_t type

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node to end of dlistint_t
+ * @head: head of list
+ * @n: element to add
+ *
+ * Return: new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *current, *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	current = *head;
+	if (!current)
+	{
+		*head = new;
+		new->next = current;
+		return (new);
+	}
+	while (current->next)
+	{
+		current = current->next;
+	}
+	printf("%d\n", current->n);
+	current->next = new;
+	new->prev = current;
+	new->next = NULL;
+	return new;
+}
