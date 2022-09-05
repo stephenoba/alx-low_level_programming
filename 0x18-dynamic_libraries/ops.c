6dbeb9d91f084b16f98a1c1563912fc43e0cd608
Stephen Oba <obastepheno@gmail.com>
2022-09-05 21:45:38 +0100

    Create shared library 100-operations.so
    Shared library is used within the python file 100-test.py

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,59 @@
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - remainder of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
