ce63fc982287b28c0f7ba0dca1f7f9150c0759e4
Stephen Oba <obastepheno@gmail.com>
2022-09-22 21:27:24 +0100

    Add function to get Items from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Get value from Hash table
+ * @ht: Hash table
+ * @key: key
+ *
+ * Return: Value or NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (strlen(key) == 0 || !key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	while(node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
