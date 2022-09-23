8a90fb977b2294a100aa0d8a0acbd56e4eff1893
Stephen Oba <obastepheno@gmail.com>
2022-09-23 21:35:06 +0100

    Added check for similar key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,16 +11,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = NULL;
+	hash_node_t *current_node, *new_node = NULL;
 
 	if (strlen(key) == 0 || !key)
 		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array[index])
 		new_node->next = ht->array[index];
 	else
